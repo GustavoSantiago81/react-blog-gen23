371e14ef403a6a3242ad80d687a5c139d6de6a15
Gustavo Santiago <gustavosantiago81@yahoo.com.br>
2023-09-30 20:27:12 -0300

    iniciando e testando props

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,34 @@
+// React Props
+// function Pai() {
+//     const dados = { nome: "João", idade: 30 };
+//     return <Filho dados={dados} />;
+//   }
+  
+//   function Filho(props) {
+//     return (
+//       <div>
+//         <p>Nome: {props.dados.nome}</p>
+//         <p>Idade: {props.dados.idade}</p>
+//       </div>
+//     );
+//   }
+
+// em TypeScript
+import React from 'react';
+import './Home.css';
+
+interface minhaProps {
+    title: string;
+    description: string;
+  }
+  
+function Home(props:minhaProps){
+    return (
+        <>
+        <h2>{props.title}</h2>
+        <p>{props.description}</p>
+        </>
+    );
+}
+
+export default Home;

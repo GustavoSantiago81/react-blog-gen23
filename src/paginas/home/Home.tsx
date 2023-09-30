eacccbba750da633122e8b69146650b7d84c2b38
Gustavo Santiago <gustavosantiago81@yahoo.com.br>
2023-09-30 20:49:58 -0300

    useState e useEffects

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -3,7 +3,7 @@
 //     const dados = { nome: "João", idade: 30 };
 //     return <Filho dados={dados} />;
 //   }
-  
+
 //   function Filho(props) {
 //     return (
 //       <div>
@@ -14,20 +14,36 @@
 //   }
 
 // em TypeScript é necessário especificar o tipo com uma interface por exemplo
-import './Home.css';
+import { useEffect, useState } from "react";
+import "./Home.css";
 
 interface minhaProps {
-    title: string;
-    description: string;
+  title: string;
+  description: string;
+}
+
+function Home(props: minhaProps) {
+  const [state, setState] = useState("initialState");
+  const [valor, setValor] = useState(0);
+
+  useEffect(() => {
+    // código para executar o efeito colateral
+  }); // }, [dependencias] );
+
+  function handleClick() {
+    setValor(valor + 1);
   }
-  
-function Home(props:minhaProps){
-    return (
-        <>
-        <h2>{props.title}</h2>
-        <p>{props.description}</p>
-        </>
-    );
+
+  return (
+    <>
+      <div>
+        <p>O valor é: {valor}</p>
+        <button onClick={handleClick}>Adicionar 1</button>
+      </div>
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+    </>
+  );
 }
 
 export default Home;

ba1f4f6398928ebe626d3f2bd246bca76d0410f2
Gustavo Santiago <gustavosantiago81@yahoo.com.br>
2023-10-01 18:53:00 -0300

    testando useNavigate e  Link

---
+++ src/paginas/PageOne.tsx
@@ -0,0 +1,9 @@
+function PageOne() {
+  return (
+    <>
+      <p>Pagina um</p>
+    </>
+  );
+}
+
+export default PageOne;
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,91 +1,25 @@
-// React Props
-// function Pai() {
-//     const dados = { nome: "João", idade: 30 };
-//     return <Filho dados={dados} />;
-//   }
+import { useNavigate, Link } from 'react-router-dom'
 
-//   function Filho(props) {
-//     return (
-//       <div>
-//         <p>Nome: {props.dados.nome}</p>
-//         <p>Idade: {props.dados.idade}</p>
-//       </div>
-//     );
-//   }
+const Home = () => {
+    let navigate = useNavigate()
+    return (
 
-// em TypeScript é necessário especificar o tipo com uma interface por exemplo
-// import { useEffect, useState } from "react";
-// import "./Home.css";
+        <div>
+            <h2 className="text-slate-900 text-5xl  m-4">Home</h2>
+            <div>
+              {/* useNavigate */}
+                <button type='submit'
+                    className='hover:underline mx-4'
+                    onClick={() => { navigate('/login') }}>
+                    Login useNavigate
+                </button>
+                {/* Componente Link */}
+                <Link to='/login' className='hover:underline mx-4'>Login por Link</Link>
+            </div>
 
-// interface minhaProps {
-//   title: string;
-//   description: string;
-// }
+        </div>
 
-// function Home(props: minhaProps) {
-//   const [state, setState] = useState("initialState");
-//   const [valor, setValor] = useState(0);
-
-//   useEffect(() => {
-//     // código para executar o efeito colateral
-//   }); // }, [dependencias] );
-
-//   function handleClick() {
-//     setValor(valor + 1);
-//   }
-
-//   return (
-//     <>
-//       <div>
-//         <p>O valor é: {valor}</p>
-//         <button onClick={handleClick}>Adicionar 1</button>
-//       </div>
-//       <h2>{props.title}</h2>
-//       <p>{props.description}</p>
-//     </>
-//   );
-// }
-
-// export default Home;
-
-
-//useEffect
-// import React, { useState, useEffect } from 'react';
-
-// function Home() {
-//   const [completed, setCompleted] = useState(false);
-//   const [tarefa, setTarefa] = useState('');
-
-//   useEffect(() => {
-//     if (completed) {
-//       setTarefa('Parabéns! Você concluiu a tarefa!');
-//     }
-//   }, [completed]);
-
-//   return (
-//     <div>
-//       <h1>Tarefa</h1>
-//       <h3>{tarefa}</h3>
-//       <p>Conclua a tarefa</p>
-//       <button onClick={() => setCompleted(true)}>Concluir Tarefa</button>
-//     </div>
-//   );
-// }
-
-// export default Home;
-
-// RENDERIZAÇÃO CONDICIONAL
-import { useState } from 'react';
-
-function Home() {
-  const [loggedIn, setLoggedIn] = useState(false);
-
-  return (
-    <div>
-      <h1 className="text-3xl font-bold text-red-500 underline text-center">
-      Hello world!
-    </h1>
-    </div>
-  );
+    )
 }
-export default Home;
+
+export default Home
